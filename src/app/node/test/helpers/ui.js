fe218fbb8a574a8df861a98cbd40110c6b34cbd6
Aco Mitevski <aco.mitevski@gmail.com>
2014-09-02 23:09:51 +0200

    improve test coverage for ui proxy

--- src/app/node/test/helpers/ui.js
+++ src/app/node/test/helpers/ui.js
@@ -25,6 +25,7 @@ describe('UI', function() {
     var b = require(root + 'helpers/ui');
     expect(a).to.equal(b);
   });
+
   describe('setPoints', function() {
     it('should emit given points', function() {
       ui.setPoints(10);
@@ -43,4 +44,43 @@ describe('UI', function() {
       expect(emitSpy).calledWith('openMenu');
     });
   });
+  describe('startSelectedGame', function() {
+    it('should emit startSelectedGame', function() {
+      ui.startSelectedGame();
+      expect(emitSpy).calledWith('startSelectedGame');
+    });
+  });
+  describe('nextGame', function() {
+    it('should emit nextGame', function() {
+      ui.nextGame();
+      expect(emitSpy).calledWith('nextGame');
+    });
+  });
+  describe('prevGame', function() {
+    it('should emit prevGame', function() {
+      ui.prevGame();
+      expect(emitSpy).calledWith('prevGame');
+    });
+  });
+  describe('setGameMessage', function() {
+    it('should emit setGameMessage with right message', function() {
+      ui.setGameMessage('foo');
+      expect(emitSpy).calledWith('setGameMessage', 'foo');
+    });
+  });
+  describe('resetGameMessage', function() {
+    it('should emit setGameMessage with empty message', function() {
+      ui.resetGameMessage();
+      expect(emitSpy).calledWith('setGameMessage', '');
+    });
+  });
+  describe('setTargets', function() {
+    it('should emit setTargets with given targets', function() {
+      var targets = {1: 'x', 2: 'y'};
+      ui.setTargets(targets);
+      expect(emitSpy).calledWith('setTargets', targets);
+    });
+  });
+
+
 });
